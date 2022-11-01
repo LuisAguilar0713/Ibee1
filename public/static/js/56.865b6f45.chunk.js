a7e093d1d5fa25b69b168519318ff32eda310865
LuisAguilar0713 <18670043@itiguala.edu.mx>
2022-11-01 13:11:03 -0600

    revertir

--- public/static/js/56.865b6f45.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpIbee1=this.webpackJsonpIbee1||[]).push([[56],{417:function(n,t,e){"use strict";e.r(t),e.d(t,"startStatusTap",(function(){return c}));var i=e(31),o=e(26),c=function(){var n=window;n.addEventListener("statusTap",(function(){Object(i.h)((function(){var t=n.innerWidth,e=n.innerHeight,c=document.elementFromPoint(t/2,e/2);if(c){var r=c.closest("ion-content");r&&new Promise((function(n){return Object(o.c)(r,n)})).then((function(){Object(i.f)((function(){return r.scrollToTop(300)}))}))}}))}))}}}]);
-//# sourceMappingURL=56.865b6f45.chunk.js.map
